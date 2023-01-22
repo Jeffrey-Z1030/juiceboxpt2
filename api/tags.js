32ed65940cc2047768decea189796de82cb4caec
Jeffrey-Z1030 <113541414+Jeffrey-Z1030@users.noreply.github.com>
2023-01-22 14:44:49 -0500

    part3 almost?

--- api/tags.js
+++ api/tags.js
@@ -2,12 +2,29 @@ const express = require('express');
 const tagsRouter = express.Router();
 
 
-const { getAllUsers, getAllTags } = require('../db');
+const { getAllPosts, getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
 
   next(); // THIS IS DIFFERENT
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  const { tagName } = req.params;
+  try {
+
+    const allPostsWithTag = await getPostsByTagName(tagName);
+
+    const userPosts = allPostsWithTag.filter(post => {
+      return post.active && (req.user && post.author.id === req.user.id);
+    });
+
+    res.send({userPosts})
+
+  } catch ({ name, message }) {
+    next({name,message})
+  }
 });
 
 tagsRouter.get('/', async (req, res) => {
